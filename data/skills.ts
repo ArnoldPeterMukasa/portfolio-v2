d732515f737b2473a1ff15f962ace12cea7416e0
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-06-12 17:15:17 +0300

    modified

---
+++ data/skills.ts
@@ -0,0 +1,14 @@
+export const skills = [
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Express",
+  "PostgreSQL",
+  "MySQL",
+  "Docker",
+  "Linux",
+  "Networking",
+  "Cloud Computing",
+];
