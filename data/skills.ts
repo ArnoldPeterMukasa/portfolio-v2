af75f90d65848f362b1291eec5d64f3567af81f4
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-06-15 10:09:19 +0300

    skills

--- data/skills.ts
+++ data/skills.ts
@@ -1,14 +1,44 @@
-export const skills = [
-  "JavaScript",
-  "TypeScript",
-  "React",
-  "Next.js",
-  "Node.js",
-  "Express",
-  "PostgreSQL",
-  "MySQL",
-  "Docker",
-  "Linux",
-  "Networking",
-  "Cloud Computing",
+export const skillsCategories =[
+  {
+    title: "Frontend",
+    skills:[
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "TypeScript",
+      "React",
+      "Next.js",
+    ],
+  },
+  {
+    title: "Backend",
+    skills:[
+      "PHP",
+      "MySQL",
+      "PostgreSQL",
+      "Node.js",
+      "Experess.js",
+    ],
+  },
+  {
+    title: "Cloud & DevOps",
+    skills:[
+      "Docker",
+      "Kubernetes",
+      "linux",
+      "Google Cloud Platform",
+    ],
+  },
+  {
+    title: "Networking & Security",
+    skills:[
+      "Networking Security",
+      "Ethical Hacking",
+      "Switching & Routing",
+      "Fortinet",
+      "Firewall",
+      "TCP/IP",
+      "Vlan",
+    ],
+  },
 ];
