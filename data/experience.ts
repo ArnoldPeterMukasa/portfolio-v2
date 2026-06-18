db56fc10a121bb4891f321410b16f028f3b85cc7
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-06-18 17:08:59 +0300

    XP

---
+++ data/experience.ts
@@ -0,0 +1,26 @@
+export const experiences=[
+    {
+        year: "2026",
+        title: "IT Intern",
+        organisation: "VINCI Construction",
+        description: "IT Operations, Machine Provisioning, System Administration, Backups, SoftWare development"
+    },
+    {
+        year: "2024-Present",
+        title: "Bachelor of Science in Computer Science",
+        organization: "Makerere University",
+        description: "Pursuing a Bachelor's Degree in Computer Science with interests in Full Stack Development, Networking and Cloud-Computing",
+    },
+    {
+        year: "2022-2024",
+        title: "Diploma in Computer Science",
+        organisation: "Makerere University Business School-(MUBS Main Campus)",
+        description: "Diploma Holder",
+    },
+    {
+        year: "2026",
+        title: "Ethical Hacking & French Journey",
+        organization: "Self-Learning",
+        description: "Studying Ethical Hacking for system secuity and French Speaking, Writing and Reading",
+    },
+];
