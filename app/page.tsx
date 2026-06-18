63c82cb8fc72dd77fe598162a83a29fe190aa3c0
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-06-18 10:14:14 +0300

    kk

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/sections/Hero";
 import About from "@/components/sections/About";
 import Skills from "@/components/sections/Skills";
 import Projects from "@/components/sections/Projects";
+import VideoShowcase from "@/components/sections/videoshowcase"; 
 export default function Home() {
   return(
     <>
@@ -11,6 +12,7 @@ export default function Home() {
       <About />
       <Skills />
       <Projects />
+      <VideoShowcase />
     </>
   );
 }
