b2d23c164b5632d30348f4697753044af28f7685
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-06-18 10:17:06 +0300

    788

---
+++ data/videos.ts
@@ -0,0 +1,9 @@
+export const videos=[
+    {
+        id:1, 
+        title: "field installation",
+        category: "field work",
+        description: "on-site infrastrucre installation and maintenance.",
+        path: "/videos/field/installation.mp4",
+    },
+];
