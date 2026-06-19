c0ba0677ffe177d4db186d4131a89620a6e08bd2
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-06-19 16:01:34 +0300

    fff

--- data/projects.ts
+++ data/projects.ts
@@ -5,12 +5,12 @@ export const projects=[
         description: 
         "A Professional Portfolio built using Next.js, TypeScript, Tailwind CSS, and Shadcn UI.",
         image: "/images/projects/arnold.png",
-        technologies:[
+        /*technologies:[
             "Next.js",
             "TypeScript",
             "Tailwind CSS",
             "Shadcn UI",
-        ],
+        ],*/
         github: "https://github.com/ArnoldPeterMukasa/portfolio-v2",
         live: "#",
         status: "In Progress",
@@ -22,7 +22,7 @@ export const projects=[
         description:
         "Designed and implemented a secure network infrastructure for a Makerere University Business School-(MUBS) and also for amedium-sized enterprise, incorporating firewalls, VPNs, and intrusion detection systems to protect sensitive data and ensure reliable connectivity.",
         image: "/images/projects/net.png",
-        technologies:[
+        /*technologies:[
             "Networking Security",
             "Ethical Hacking",
             "Vlan",
@@ -30,7 +30,7 @@ export const projects=[
         ],
         github: "#",
         live: "#",
-        status: "Completed",
+        status: "Completed",*/
         collaborators: [],
     },
     {
@@ -39,30 +39,30 @@ export const projects=[
         description:
         "System developed by group of students from Makerere University who are very passionate in coding and solving real-world problems for the betterment of eachother in the University and outside",
         image: "/images/projects/net.png",
-        technologies:[
+        /*technologies:[
             "Python-Django",
             "React",
             "PostMan",
             "JavaScript",
         ],
-        github: "#",
+        github: "#",*/
         live: "https://aits-group-g-gcyp.vercel.app/",
         status: "Completed",
         collaborators: [1, 2, 3, 4],
     },
     {
-         id: 3,
+         id: 4,
         title: "Reclaim Space Project",
         description:
         "Designed and implemented by a Group for innovators and to sloving country wide effect of loosing National IDs, People getting lost from unknown geolocation or having brain problems and get lost from home and not hearing from them",
         image: "/images/projects/net.png",
-        technologies:[
+        /*technologies:[
             "Flutter",
             "Dart Language",
             "Backend with Firebase",
             "Statellite GPS",
         ],
-        github: "#",
+        github: "#",*/
         live: "https://reclaim-space-df557.web.app/",
         status: "Completed",
         collaborators: [1, 2, 3, 5],
