a63c09237c3a3af8125740c0a5aa81031e08b682
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-06-22 14:59:54 +0300

    ggg

--- components/sections/Experience.tsx
+++ components/sections/Experience.tsx
@@ -1,33 +1,37 @@
 import {experiences} from "@/data/experience";
+import FadeIn from "../ui/FadeIn";
 export default function Experience(){
     return (
-        <section
-        id="experience"
-        className="py-24 px-6"
-        >
-            <div className="max-z-5xl mx-auto">
-                <h2 className="text-4xl font-bold mb-12">
-                    EXperience
-                </h2>
-                <div className="space-y-8">
-                    {experiences.map((exp, index)=>(
-                        <div
-                        key={index}
+        <FadeIn>
+            <section
+            id="experience"
+            className="py-24 px-6"
+            >
+                <div className="max-z-5xl mx-auto">
+                    <h2 className="text-4xl font-bold mb-12">
+                        EXperience
+                    </h2>
+                    <div className="space-y-8">
+                        {experiences.map((exp, index)=>(
+                            <div
+                            key={index}
                         className="border-l-4 border-cyan-500 pl-6"
-                        >
-                            <h3 className="text-2xl font-semibold">
-                                {exp.title}
-                            </h3>
-                            <h4 className="font-meduim text-cyan-400">
-                                {exp.organisation}
-                            </h4>
-                            <p className="mt-2 text-muted-foreground">
-                                {exp.description}
-                            </p>
-                        </div>
-                    ))}
+                             >
+                                <h3 className="text-2xl font-semibold">
+                                    {exp.title}
+                                </h3>
+                                <h4 className="font-meduim text-cyan-400">
+                                    {exp.organisation}
+                                </h4>
+                                <p className="mt-2 text-muted-foreground">
+                                    {exp.description}
+                                </p>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </FadeIn>
     );
 }
+ 
