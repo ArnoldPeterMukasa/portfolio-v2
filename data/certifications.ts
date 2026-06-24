d541688b77076844ccd89b47f7fd8835105b01dd
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-06-24 14:40:47 +0300

    dddd

---
+++ data/certifications.ts
@@ -0,0 +1,28 @@
+export const certifications = [
+  {
+    id: 1,
+    title: "CCNA Introduction to Networks",
+    issuer: "Cisco Networking Academy",
+    date: "2025",
+    image: "/images/certificates/cisco.png",
+    link: "#",
+  },
+
+  {
+    id: 2,
+    title: "Cybersecurity Essentials",
+    issuer: "Cisco Networking Academy",
+    date: "2025",
+    image: "/images/certificates/cybersecurity.png",
+    link: "#",
+  },
+
+  {
+    id: 3,
+    title: "Fortinet NSE",
+    issuer: "Fortinet",
+    date: "Coming Soon",
+    image: "/images/certificates/fortinet.png",
+    link: "#",
+  },
+];
