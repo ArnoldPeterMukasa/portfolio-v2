c20a8834313d276f3569aad673b76a65aa90be76
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-07-03 17:48:45 +0300

    ppp

---
+++ data/stats.ts
@@ -0,0 +1,34 @@
+export const stats = [
+  {
+    id: 1,
+    value: "15+",
+    label: {
+      en: "Projects Completed",
+      fr: "Projets Réalisés",
+    },
+  },
+  {
+    id: 2,
+    value: "3+",
+    label: {
+      en: "Years of Learning",
+      fr: "Années d'Apprentissage",
+    },
+  },
+  {
+    id: 3,
+    value: "8+",
+    label: {
+      en: "Technologies",
+      fr: "Technologies",
+    },
+  },
+  {
+    id: 4,
+    value: "100%",
+    label: {
+      en: "Commitment",
+      fr: "Engagement",
+    },
+  },
+];
