be9e909c059cbcaa90893a819497c86594dca91a
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-07-09 14:58:59 +0300

    offf

---
+++ app/videos/page.tsx
@@ -0,0 +1,9 @@
+import VideoShowcase from "@/components/sections/VideoShowcase";
+
+export default function VideosPage() {
+  return (
+    <main className="px-6 py-24">
+      <VideoShowcase />
+    </main>
+  );
+}
