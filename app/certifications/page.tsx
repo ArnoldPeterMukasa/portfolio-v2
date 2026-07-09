6c4acb980df1f34be97ad91e2a83d56f82c70c64
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-07-09 14:57:24 +0300

    offf

---
+++ app/certifications/page.tsx
@@ -0,0 +1,9 @@
+import Certifications from "@/components/sections/Certifications";
+
+export default function CertificationsPage() {
+  return (
+    <main className="px-6 py-24">
+      <Certifications />
+    </main>
+  );
+}
