ca3012bbe958f5672c96851fc63d2707177cd67a
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-07-09 14:58:25 +0300

    offf

---
+++ app/projects/page.tsx
@@ -0,0 +1,9 @@
+import Projects from "@/components/sections/Projects";
+
+export default function ProjectsPage() {
+  return (
+    <main className="px-6 py-24">
+      <Projects />
+    </main>
+  );
+}
