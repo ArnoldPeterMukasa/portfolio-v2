dca95af89bcad40a80bd11cd4ed6c99554d1f9b8
ArnoldPeterMukasa <info.ap.mukasa@gmail.com>
2026-07-09 14:58:00 +0300

    offf

---
+++ app/experience/page.tsx
@@ -0,0 +1,9 @@
+import Experience from "@/components/sections/Experience";
+
+export default function ExperiencePage() {
+  return (
+    <main className="px-6 py-24">
+      <Experience />
+    </main>
+  );
+}
